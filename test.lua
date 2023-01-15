20f06c6ffc216ff394b44c5a18b56f904f0ce4ee
maincreams <121547440+maincreams@users.noreply.github.com>
2023-01-15 14:59:10 +1000

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,294 @@
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local blackGui = Instance.new("ScreenGui")
+local blackFrame = Instance.new("Frame")
+blackFrame.Size = UDim2.new(1, 0, 1, 0)
+blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+blackFrame.Parent = blackGui
+blackGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.02)
+
+local player = game.Players.LocalPlayer
+local greenGui = Instance.new("ScreenGui")
+local greenFrame = Instance.new("Frame")
+greenFrame.Size = UDim2.new(1, 0, 1, 0)
+greenFrame.BackgroundColor3 = Color3.new(0, 1, 0)
+greenFrame.Parent = greenGui
+greenGui.Parent = player:WaitForChild("PlayerGui")
+
+wait(0.2)
+
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+
+player:Kick("Roblox Account has been terminated due to detected Exploits.")
